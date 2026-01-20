a1abf6495c37795e9a052cf513337a192a283683
Guilherme <guilhermeperes0202@gmail.com>
2026-01-19 21:06:30 -0300

    pacote domain-Delivery

---
+++ Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/domain/model/Delivery.java
@@ -0,0 +1,21 @@
+package com.guilherme.deliveryservice.delivery.tracking.domain.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class Delivery {
+    private UUID id;
+    private UUID courierId;
+
+    private OffsetDateTime placedAt;
+    private OffsetDateTime assignedAt;
+    private OffsetDateTime expectedDeliveryAt;
+    private OffsetDateTime fulfilledAt;
+
+    private BigDecimal distanceFree;
+    private BigDecimal courierPayout;
+    private BigDecimal totalCosy;
+
+    private Integer totalItems;
+}
---
+++ Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/domain/model/DeliveryStatus.java
@@ -0,0 +1,4 @@
+package com.guilherme.deliveryservice.delivery.tracking.domain.model;
+
+public class DeliveryStatus {
+}
