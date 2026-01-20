de06c6a59ad8f42c98ddebb9eecac0ff0dc5cf8d
Guilherme <guilhermeperes0202@gmail.com>
2026-01-19 21:43:35 -0300

    pacote domain-Item (factory)

--- Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/domain/model/Item.java
+++ Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/domain/model/Item.java
@@ -1,7 +1,12 @@
 package com.guilherme.deliveryservice.delivery.tracking.domain.model;
 
 import java.util.UUID;
+
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 
@@ -10,4 +15,12 @@ public class Item {
     private UUID id;
     private String name;
     private Integer quantity;
+
+    static Item brandNew(String name, Integer quantity){
+        Item item = new Item();
+        item.id = UUID.randomUUID();
+        item.name = name;
+        item.quantity = quantity;
+        return item;
+    }
 }
