582ad038df921f5eedda04f6a6317794819d2e9a
Guilherme <guilhermeperes0202@gmail.com>
2026-02-02 16:53:42 -0300

    Persistence entity

--- Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/domain/model/Item.java
+++ Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/domain/model/Item.java
@@ -2,8 +2,12 @@ package com.guilherme.deliveryservice.delivery.tracking.domain.model;
 
 import java.util.UUID;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
+@Entity
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Setter(AccessLevel.PRIVATE)
@@ -13,18 +17,25 @@ import lombok.*;
 
 
 public class Item {
+
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
     private String name;
     @Setter(AccessLevel.PACKAGE)
     private Integer quantity;
 
+    @ManyToOne(optional = false)
+    @Getter(AccessLevel.PRIVATE)
+    private Delivery delivery;
 
-    static Item brandNew(String name, Integer quantity){
+
+    static Item brandNew(String name, Integer quantity, Delivery delivery){
         Item item = new Item();
         item.setId(UUID.randomUUID());
         item.setName(name);
         item.setQuantity(quantity);
+        item.setDelivery(delivery);
         return item;
     }
 }
