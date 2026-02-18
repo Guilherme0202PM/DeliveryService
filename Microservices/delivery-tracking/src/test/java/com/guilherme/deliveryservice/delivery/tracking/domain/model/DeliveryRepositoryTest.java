65ad6913417893b652820da62e43eeb05fb2feb4
Guilherme <guilhermeperes0202@gmail.com>
2026-02-18 15:28:00 -0300

    Criando Aplication.yml para teste

---
+++ Microservices/delivery-tracking/src/test/java/com/guilherme/deliveryservice/delivery/tracking/domain/model/DeliveryRepositoryTest.java
@@ -0,0 +1,11 @@
+package com.guilherme.deliveryservice.delivery.tracking.domain.model;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DeliveryRepositoryTest {
+}
