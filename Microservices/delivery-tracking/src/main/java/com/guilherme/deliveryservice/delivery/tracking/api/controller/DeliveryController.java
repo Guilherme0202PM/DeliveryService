c467d79a04dfbcf7a0a9ef6dcd987646a02fcd2a
Guilherme <guilhermeperes0202@gmail.com>
2026-02-25 20:58:52 -0300

    Domain Service - CourierController

--- Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/api/controller/DeliveryController.java
+++ Microservices/delivery-tracking/src/main/java/com/guilherme/deliveryservice/delivery/tracking/api/controller/DeliveryController.java
@@ -14,7 +14,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.hateoas.PagedModel;
 
 import java.util.UUID;
 
