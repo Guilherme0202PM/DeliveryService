b8d6db5160376b09623ab54d387437b730487ba4
Guilherme <guilhermeperes0202@gmail.com>
2026-02-25 21:25:09 -0300

    Domain Service - PageModel True 2.0

--- Microservices/delivery-manager/src/main/java/com/guilherme/deliveryservice/delivery/manager/api/controller/CourierController.java
+++ Microservices/delivery-manager/src/main/java/com/guilherme/deliveryservice/delivery/manager/api/controller/CourierController.java
@@ -12,7 +12,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.bind.annotation.PagedModel;
+//import org.springframework.web.bind.annotation.PagedModel;
 import org.springframework.hateoas.PagedModel;
 
 
